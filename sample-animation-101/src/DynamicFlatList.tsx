e54f3e8b164f2c554642ba99fb03bdd517c11436
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-02-18 23:55:42 +0900

    Add Types

--- sample-animation-101/src/DynamicFlatList.tsx
+++ sample-animation-101/src/DynamicFlatList.tsx
@@ -1,16 +1,11 @@
-import React, { useCallback } from 'react';
-import { StatusBar, StyleSheet, FlatList, View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import {StatusBar, StyleSheet, FlatList, View, Text} from 'react-native';
 import chroma from 'chroma-js';
 import Animated, {
-  useDerivedValue,
   useSharedValue,
   useAnimatedStyle,
   useAnimatedScrollHandler,
-  withTiming,
   interpolate,
-  measure,
-  useAnimatedRef,
-  Extrapolate,
 } from 'react-native-reanimated';
 import Constants from 'expo-constants';
 
@@ -20,7 +15,13 @@ const colors = chroma.scale(['#fafa6e', '#2A4858']).mode('lch').colors(30);
 
 const _spacing = 10;
 
-const _data = colors.map((color) => {
+interface DataProps {
+  key: number;
+  color: string;
+  height: number;
+}
+
+const _data: DataProps[] = colors.map((color) => {
   return {
     key: Date.now(),
     color: color,
@@ -30,7 +31,7 @@ const _data = colors.map((color) => {
 
 const _offset = 0;
 
-const Item = ({ item, index, scrollY, itemY, itemHeight }: any) => {
+const Item = ({item, index, scrollY, itemY, itemHeight}: any) => {
   console.log('from children y:', itemY);
   const stylez = useAnimatedStyle(() => {
     // console.log(values.value)
@@ -38,12 +39,12 @@ const Item = ({ item, index, scrollY, itemY, itemHeight }: any) => {
     return {
       opacity: interpolate(scrollY.value, [itemY.value - 1, itemY.value, itemY.value + itemHeight.value], [1, 1, 0]),
       transform: [
-        { perspective: itemHeight.value * 4 },
+        {perspective: itemHeight.value * 4},
         {
           translateY: interpolate(
-            scrollY.value,
-            [itemY.value - index * _offset - 1, itemY.value - index * _offset, itemY.value - index * _offset + 1],
-            [0, 0, 1],
+              scrollY.value,
+              [itemY.value - index * _offset - 1, itemY.value - index * _offset, itemY.value - index * _offset + 1],
+              [0, 0, 1],
           ),
         },
         {
@@ -56,63 +57,64 @@ const Item = ({ item, index, scrollY, itemY, itemHeight }: any) => {
     };
   });
   return (
-    <Animated.View
-      style={[
-        {
-          backgroundColor: item.color,
-          marginBottom: _spacing,
-          height: item.height,
-          padding: _spacing,
-          borderRadius: 16,
-          justifyContent: 'flex-end',
-          alignItems: 'flex-end',
-        },
-        stylez,
-      ]}>
-      <Text style={{ fontWeight: '700', textTransform: 'uppercase' }}>{item.color}</Text>
-    </Animated.View>
+      <Animated.View
+          style={[
+            {
+              backgroundColor: item.color,
+              marginBottom: _spacing,
+              height: item.height,
+              padding: _spacing,
+              borderRadius: 16,
+              justifyContent: 'flex-end',
+              alignItems: 'flex-end',
+            },
+            stylez,
+          ]}>
+        <Text style={{fontWeight: '700', textTransform: 'uppercase'}}>{item.color}</Text>
+      </Animated.View>
   );
 };
 
-const CustomCellRendererComponent = React.memo(({ children, ...props }) => {
+const CustomCellRendererComponent = React.memo(({children, ...props}) => {
   const itemY = useSharedValue(0);
   const itemHeight = useSharedValue(0);
   return (
-    <View
-      {...props}
-      onLayout={(ev) => {
-        itemY.value = ev.nativeEvent.layout.y;
-        itemHeight.value = ev.nativeEvent.layout.height;
-      }}>
-      {React.Children.map(children, (child) => {
-        if (React.isValidElement(child)) {
-          return React.cloneElement(child, { itemY, itemHeight });
-        }
-        return child;
-      })}
-    </View>
+      <View
+          {...props}
+          onLayout={(ev) => {
+            itemY.value = ev.nativeEvent.layout.y;
+            itemHeight.value = ev.nativeEvent.layout.height;
+          }}>
+        {React.Children.map(children, (child) => {
+          if (React.isValidElement(child)) {
+            return React.cloneElement(child, {itemY, itemHeight});
+          }
+          return child;
+        })}
+      </View>
   );
 });
 
 const DynamicFlatList = () => {
   const scrollY = useSharedValue(0);
-  const onScroll = useAnimatedScrollHandler((ev) => {
-    scrollY.value = ev.contentOffset.y;
+  const onScroll = useAnimatedScrollHandler(({contentOffset: {y}}) => {
+    scrollY.value = y;
   });
+
   return (
-    <View style={styles.container}>
-      <StatusBar hidden />
-      <AnimatedFlatList
-        data={_data}
-        keyExtractor={(item) => item.key}
-        onScroll={onScroll}
-        scrollEventThrottle={16}
-        CellRendererComponent={CustomCellRendererComponent}
-        renderItem={({ item, index }) => {
-          return <Item item={item} index={index} scrollY={scrollY} />;
-        }}
-      />
-    </View>
+      <View style={styles.container}>
+        <StatusBar hidden/>
+        <AnimatedFlatList
+            data={_data}
+            keyExtractor={(item, index) => `_${index}`}
+            onScroll={onScroll}
+            scrollEventThrottle={16}
+            CellRendererComponent={CustomCellRendererComponent}
+            renderItem={({item, index}) => {
+              return <Item item={item} index={index} scrollY={scrollY}/>;
+            }}
+        />
+      </View>
   );
 };
 
